8fe5ce69679844ebcf525b22b914c3096aafa822
Joseph Ligier <joseph.ligier@accenture.com>
2021-08-13 13:11:13 +0200

    terraform fmt

--- lambda/variables.tf
+++ lambda/variables.tf
@@ -1,24 +1,24 @@
-variable function_name {
-  type = string
+variable "function_name" {
+  type        = string
   description = "name of lambda function"
 }
 
-variable family {
-  type = string
+variable "family" {
+  type        = string
   description = "family of task definition with revision: format: $family:$revision"
 }
 
-variable container_name {
-  type = string
+variable "container_name" {
+  type        = string
   description = "name of container you want to launch"
 }
 
-variable default_sg {
-  type = string
+variable "default_sg" {
+  type        = string
   description = "id of security group you want to launch the container"
 }
 
-variable default_subnet {
-  type = string
+variable "default_subnet" {
+  type        = string
   description = "id of subnet you want to launch the container"
 }
