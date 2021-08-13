6ee1c1011b3f275200df677dd9647b537b5fb260
Joseph Ligier <joseph.ligier@accenture.com>
2021-08-13 13:11:25 +0200

    terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "ecs_cluster_name" {
-  default = "unzip-cluster"
-  type = string
+  default     = "unzip-cluster"
+  type        = string
   description = "Name of ecs cluster where you launch ecs task"
 }
 
 variable "container_image" {
-  default = "littlejo/unzip-s3:bash"
-  type = string
+  default     = "littlejo/unzip-s3:bash"
+  type        = string
   description = "Name of docker hub repo"
 }
 
 variable "container_name" {
-  default = "unzip"
-  type = string
+  default     = "unzip"
+  type        = string
   description = "Name of container of ecs task"
 }
 
 variable "awslogs_group" {
-  default = "/ecs-cluster/services/unzip"
-  type = string
+  default     = "/ecs-cluster/services/unzip"
+  type        = string
   description = "Logs group name of task"
 }
 
 variable "family" {
-  default = "unzip"
-  type = string
+  default     = "unzip"
+  type        = string
   description = "Family of task definition"
 }
 
 variable "lambda_function_name" {
-  default = "create_zip"
-  type = string
+  default     = "create_zip"
+  type        = string
   description = "Name of lambda function"
 }
